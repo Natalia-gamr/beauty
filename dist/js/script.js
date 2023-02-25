69e2e51fff983549cc80f1ad4c8fe4e43261952d
Natalia <77733802+Natalia-gamr@users.noreply.github.com>
2023-02-25 15:44:41 +0300

    add form

--- dist/js/script.js
+++ dist/js/script.js
@@ -8,4 +8,44 @@ function closeModal() {
 function openModal() {
     modal.style.display = 'flex';
 }
-      
+
+function submitModal() {
+    const form = document.querySelector('.modal__form');
+
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let tel = document.querySelector('#tel').value;
+        let message = document.querySelector('#message').value;
+
+        console.log(tel);
+        console.log(message);
+    })
+
+    let alert = document.createElement('div');
+    alert.className = 'modal__success';
+    alert.append('Спасибо, наш оператор свяжется с вами в ближайшее время!');
+    form.append(alert)
+    
+    setTimeout(() => {
+        closeModal();
+        alert.remove();
+        form.reset();
+    }, 5000);
+}
+
+function subscribe() {
+    const subscribe = document.querySelector('#subscribe');
+
+    subscribe.addEventListener('submit', (e) => {
+        e.preventDefault();
+        let email = document.querySelector('#mail').value;
+
+        console.log(email);
+    })
+    setTimeout(() => {
+        subscribe.reset();
+    }, 3500); 
+}
+
+
+   
